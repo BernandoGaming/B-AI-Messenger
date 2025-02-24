1e90bf13c8aebac289aa49ef78bc052c64743665
BernandoGaming <138401118+BernandoGaming@users.noreply.github.com>
2025-02-24 13:00:54 +0700

    Update normal ini mah

--- B-AI.js
+++ B-AI.js
@@ -1,16 +1,16 @@
 {
-  "nama": "Ayanokoji Kiyotaka",
-  "awalan": ".",
+  "botName": "B-AI Messenger",
+  "prefix": ".",
   "port": 8080,
   "proxy": "103.179.217.242",
-  "bahasa": "id",
-  "notifkey": "Z6Iy6fEvjDNimKkB",
-  "aikey": "AIzaSyAXBvwNkZBckC8b3vTZVVuwRwlsPttTgkA",
+  "language": "en",
+  "notificationKey": "Z6Iy6fEvjDNimKkB",
+  "aiKey": "AIzaSyAXBvwNkZBckC8b3vTZVVuwRwlsPttTgkA",
   "chatdm": false,
-  "maintain": false,
-  "zonawaktu": "Asia/Jakarta", 
-  "admin": [""], 
-  "setting": {
+  "maintenance": false,
+  "timezone": "Asia/Jakarta",
+  "admin": [""],
+  "settings": {
     "forceLogin": true,
     "listenEvents": true,
     "updatePresence": true,
@@ -20,5 +20,5 @@
     "selfListenEvent": true,
     "autoMarkDelivery": false,
     "autoReconnect": false
- }
+  }
 }
