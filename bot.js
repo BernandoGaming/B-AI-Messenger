94ef366d05c1810153567a0d45f1d7cad74ce3fe
BernandoGaming <138401118+BernandoGaming@users.noreply.github.com>
2025-02-25 04:06:15 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const login = require('./system/auth');
+const login = require('./system/fb-api');
 const { logo, colors, fonts, bai } = require('./system/log');
 const fs = require('fs');
 const path = require('path');
